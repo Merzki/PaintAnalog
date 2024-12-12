bf7e72a8130641afe4613dd182387f94b6c4fe39
Merzki <guforski@gmail.com>
2024-12-12 07:56:07 +0100

    pushing every file :)

---
+++ PaintAnalog/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using PaintAnalog.ViewModels;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Shapes;
+
+namespace PaintAnalog
+{
+    public partial class MainWindow : Window
+    {
+        private Polyline _currentLine;
+        private MainViewModel ViewModel => DataContext as MainViewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                if (ViewModel?.SelectedColor != null)
+                {
+                    _currentLine = new Polyline
+                    {
+                        Stroke = ViewModel.SelectedColor,
+                        StrokeThickness = 2
+                    };
+                    _currentLine.Points.Add(e.GetPosition(PaintCanvas));
+                    PaintCanvas.Children.Add(_currentLine);
+                }
+            }
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_currentLine != null && e.LeftButton == MouseButtonState.Pressed)
+            {
+                _currentLine.Points.Add(e.GetPosition(PaintCanvas));
+            }
+        }
+
+        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            _currentLine = null;
+        }
+    }
+}
---
+++ PaintAnalog/ViewModels/MainViewModel.cs
@@ -0,0 +1,106 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Microsoft.Win32;
+using System.Windows.Media.Imaging;
+using Xceed.Wpf.Toolkit;
+
+namespace PaintAnalog.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _title = "Paint Analog";
+        private SolidColorBrush _selectedColor = new SolidColorBrush(Colors.Black);
+
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public SolidColorBrush SelectedColor
+        {
+            get => _selectedColor;
+            set => SetProperty(ref _selectedColor, value);
+        }
+
+        public ICommand ClearCanvasCommand { get; }
+        public ICommand ChooseColorCommand { get; }
+        public ICommand SaveCanvasCommand { get; }
+
+        public MainViewModel()
+        {
+            ClearCanvasCommand = new RelayCommand(ClearCanvas);
+            ChooseColorCommand = new RelayCommand(ChooseColor);
+            SaveCanvasCommand = new RelayCommand(SaveCanvas);
+        }
+
+        private void ClearCanvas(object parameter)
+        {
+            var canvas = parameter as Canvas;
+            canvas?.Children.Clear();
+        }
+
+        private void ChooseColor(object parameter)
+        {
+            var colorDialog = new Xceed.Wpf.Toolkit.ColorPicker
+            {
+                SelectedColor = SelectedColor.Color 
+            };
+
+            var colorPickerPopup = new System.Windows.Controls.Primitives.Popup
+            {
+                Placement = System.Windows.Controls.Primitives.PlacementMode.Center,
+                PlacementTarget = parameter as System.Windows.UIElement, 
+                StaysOpen = false,
+                Child = colorDialog, 
+                IsOpen = true 
+            };
+
+            colorDialog.SelectedColorChanged += (s, e) =>
+            {
+                if (colorDialog.SelectedColor.HasValue)
+                {
+                    SelectedColor = new SolidColorBrush(colorDialog.SelectedColor.Value);
+                }
+                else
+                {
+                    SelectedColor = new SolidColorBrush(Colors.Black);
+                }
+                colorPickerPopup.IsOpen = false;
+            };
+        }
+
+
+
+        private void SaveCanvas(object parameter)
+        {
+            var canvas = parameter as Canvas;
+            if (canvas == null) return;
+
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = "PNG Image|*.png",
+                Title = "Save Canvas As Image"
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                var renderBitmap = new RenderTargetBitmap(
+                    (int)canvas.ActualWidth,
+                    (int)canvas.ActualHeight,
+                    96, 96, PixelFormats.Pbgra32);
+
+                renderBitmap.Render(canvas);
+
+                var encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(renderBitmap));
+
+                using (var stream = System.IO.File.Create(saveFileDialog.FileName))
+                {
+                    encoder.Save(stream);
+                }
+            }
+        }
+    }
+}
