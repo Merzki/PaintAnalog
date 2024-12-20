e229b2304dc86c9829923ea246720ff83d23ee46
Merzki <guforski@gmail.com>
2024-12-20 06:01:29 +0100

    adding brush settings menu, which give us possibility to change brush thickness and shape

--- PaintAnalog/MainWindow.xaml.cs
+++ PaintAnalog/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 using PaintAnalog.ViewModels;
+using PaintAnalog.Views;
 
 namespace PaintAnalog
 {
@@ -11,11 +12,14 @@ namespace PaintAnalog
     {
         private bool _isDrawing;
         private Point _startPoint;
-        private Polyline _currentPolyline;
+        private Shape _currentShapeElement;
 
-        private Image _selectedImage;
-        private Point _imageStartPoint;
+        private UIElement _selectedElement; 
+        private Point _elementStartPoint;
         private Point _canvasStartOffset;
+
+        private double _currentThickness = 2.0;
+        private string _currentShape = "Polyline";
 
         private MainViewModel ViewModel => DataContext as MainViewModel;
 
@@ -33,11 +37,11 @@ namespace PaintAnalog
             {
                 var clickedElement = e.OriginalSource as UIElement;
 
-                if (clickedElement is Image image)
+                if (clickedElement != null && (clickedElement is Image || clickedElement is Shape))
                 {
-                    _selectedImage = image;
-                    _imageStartPoint = position;
-                    _canvasStartOffset = new Point(Canvas.GetLeft(image), Canvas.GetTop(image));
+                    _selectedElement = clickedElement;
+                    _elementStartPoint = position;
+                    _canvasStartOffset = new Point(Canvas.GetLeft(clickedElement), Canvas.GetTop(clickedElement));
                 }
                 else
                 {
@@ -46,14 +50,11 @@ namespace PaintAnalog
                     _isDrawing = true;
                     _startPoint = position;
 
-                    _currentPolyline = new Polyline
-                    {
-                        Stroke = ViewModel?.SelectedColor ?? Brushes.Black,
-                        StrokeThickness = 2,
-                        Points = new PointCollection { _startPoint }
-                    };
+                    _currentShapeElement = CreateShape(_currentShape, _startPoint, _startPoint);
+                    _currentShapeElement.Stroke = ViewModel?.SelectedColor ?? Brushes.Black;
+                    _currentShapeElement.StrokeThickness = _currentThickness;
 
-                    PaintCanvas.Children.Add(_currentPolyline);
+                    PaintCanvas.Children.Add(_currentShapeElement);
                 }
             }
         }
@@ -62,35 +63,144 @@ namespace PaintAnalog
         {
             var position = e.GetPosition(PaintCanvas);
 
-            if (_selectedImage != null && e.LeftButton == MouseButtonState.Pressed)
+            if (_selectedElement != null && e.LeftButton == MouseButtonState.Pressed)
             {
-                var offsetX = position.X - _imageStartPoint.X;
-                var offsetY = position.Y - _imageStartPoint.Y;
+                var offsetX = position.X - _elementStartPoint.X;
+                var offsetY = position.Y - _elementStartPoint.Y;
 
-                Canvas.SetLeft(_selectedImage, _canvasStartOffset.X + offsetX);
-                Canvas.SetTop(_selectedImage, _canvasStartOffset.Y + offsetY);
+                Canvas.SetLeft(_selectedElement, _canvasStartOffset.X + offsetX);
+                Canvas.SetTop(_selectedElement, _canvasStartOffset.Y + offsetY);
             }
             else if (_isDrawing && e.LeftButton == MouseButtonState.Pressed)
             {
-                _currentPolyline?.Points.Add(position);
+                UpdateShape(_currentShapeElement, _startPoint, position);
             }
         }
 
         private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            if (_selectedImage != null)
+            if (_selectedElement != null)
             {
                 ViewModel?.SaveState(PaintCanvas);
-                _selectedImage = null;
+                _selectedElement = null;
             }
 
             if (_isDrawing && e.LeftButton == MouseButtonState.Released)
             {
                 _isDrawing = false;
-                _currentPolyline = null;
+                _currentShapeElement = null;
 
                 ViewModel?.SaveState(PaintCanvas);
             }
         }
+
+        private void OpenPenSettings(object sender, RoutedEventArgs e)
+        {
+            var penSettingsWindow = new PenSettingsWindow(Brushes.Black, _currentThickness);
+            if (penSettingsWindow.ShowDialog() == true)
+            {
+                _currentThickness = penSettingsWindow.SelectedThickness;
+                _currentShape = penSettingsWindow.SelectedShape;
+            }
+        }
+
+        private Shape CreateShape(string shapeType, Point startPoint, Point endPoint)
+        {
+            switch (shapeType)
+            {
+                case "Rectangle":
+                    return new Rectangle
+                    {
+                        Width = 0,
+                        Height = 0,
+                        Stroke = Brushes.Black,
+                        StrokeThickness = _currentThickness
+                    };
+                case "Ellipse":
+                    return new Ellipse
+                    {
+                        Width = 0,
+                        Height = 0,
+                        Stroke = Brushes.Black,
+                        StrokeThickness = _currentThickness
+                    };
+                case "Polyline":
+                    return new Polyline
+                    {
+                        Stroke = Brushes.Black,
+                        StrokeThickness = _currentThickness,
+                        Points = new PointCollection { startPoint }
+                    };
+                case "Triangle":
+                    return new Polygon
+                    {
+                        Stroke = Brushes.Black,
+                        StrokeThickness = _currentThickness,
+                        Fill = Brushes.Transparent,
+                        Points = new PointCollection { startPoint, startPoint, startPoint } 
+                    };
+                default:
+                    return new Rectangle
+                    {
+                        Width = 0,
+                        Height = 0,
+                        Stroke = Brushes.Black,
+                        StrokeThickness = _currentThickness
+                    };
+            }
+        }
+
+        private void UpdateShape(Shape shape, Point startPoint, Point endPoint)
+        {
+            if (shape is Rectangle rectangle)
+            {
+                var x = Math.Min(startPoint.X, endPoint.X);
+                var y = Math.Min(startPoint.Y, endPoint.Y);
+                var width = Math.Abs(startPoint.X - endPoint.X);
+                var height = Math.Abs(startPoint.Y - endPoint.Y);
+
+                rectangle.Width = width;
+                rectangle.Height = height;
+
+                Canvas.SetLeft(rectangle, x);
+                Canvas.SetTop(rectangle, y);
+            }
+            else if (shape is Ellipse ellipse)
+            {
+                var x = Math.Min(startPoint.X, endPoint.X);
+                var y = Math.Min(startPoint.Y, endPoint.Y);
+                var width = Math.Abs(startPoint.X - endPoint.X);
+                var height = Math.Abs(startPoint.Y - endPoint.Y);
+
+                ellipse.Width = width;
+                ellipse.Height = height;
+
+                Canvas.SetLeft(ellipse, x);
+                Canvas.SetTop(ellipse, y);
+            }
+            else if (shape is Polyline polyline)
+            {
+                polyline.Points.Add(endPoint);
+            }
+            else if (shape is Polygon polygon && _currentShape == "Triangle")
+            {
+                var baseMidpoint = new Point(
+                    (startPoint.X + endPoint.X) / 2, 
+                    endPoint.Y 
+                );
+
+                var height = Math.Abs(endPoint.X - startPoint.X) / 2; 
+                var vertex = new Point(
+                    baseMidpoint.X, 
+                    startPoint.Y - height 
+                );
+
+                polygon.Points.Clear();
+                polygon.Points.Add(startPoint);  
+                polygon.Points.Add(endPoint);    
+                polygon.Points.Add(vertex);      
+            }
+        }
+
     }
 }
---
+++ PaintAnalog/Views/PenSettingsWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PaintAnalog.Views
+{
+    public partial class PenSettingsWindow : Window
+    {
+        public string SelectedShape { get; set; } = "Polyline";
+        public Brush SelectedBrush { get; private set; } = Brushes.Black;
+        public double SelectedThickness { get; private set; } = 2.0;
+
+        public PenSettingsWindow(Brush currentBrush, double currentThickness)
+        {
+            InitializeComponent();
+
+            SelectedBrush = currentBrush;
+            SelectedThickness = currentThickness;
+
+            ThicknessSlider.Value = currentThickness;
+
+            DataContext = this;
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedThickness = ThicknessSlider.Value;
+            SelectedShape = (ShapeComboBox.SelectedItem as ComboBoxItem)?.Tag as string ?? "Polyline";
+
+            DialogResult = true;
+            Close();
+        }
+    }
+}
